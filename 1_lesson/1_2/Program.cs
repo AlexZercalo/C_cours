f0f7bd6ce55eb7e85bb7740fec3d1e9935ed6429
unknown <mir@example.com>
2022-07-13 10:03:22 +0300

    task1_2

---
+++ 1_lesson/1_2/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Write a number a: ");
+int a = int.Parse (Console.ReadLine());
+
+Console.WriteLine("Write a number b: ");
+int b = int.Parse (Console.ReadLine());
+
+
+
+if (a * a == b)
+Console.WriteLine("Yes");
+else
+Console.WriteLine("no");
