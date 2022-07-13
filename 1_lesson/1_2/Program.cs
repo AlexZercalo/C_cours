f1cb7ea33ef79e34d34e5850897fe0a1e086aee6
unknown <mir@example.com>
2022-07-13 10:20:40 +0300

    task1_3

--- 1_lesson/1_2/Program.cs
+++ 1_lesson/1_2/Program.cs
@@ -1,13 +1,11 @@
 ﻿
 Console.WriteLine("Write a number a: ");
-int a = int.Parse (Console.ReadLine());
+int a = int.Parse(Console.ReadLine());
 
 Console.WriteLine("Write a number b: ");
-int b = int.Parse (Console.ReadLine());
-
-
+int b = int.Parse(Console.ReadLine());
 
 if (a * a == b)
-Console.WriteLine("Yes");
+Console.WriteLine("Да");
 else
-Console.WriteLine("no");
+Console.WriteLine("Нет");
