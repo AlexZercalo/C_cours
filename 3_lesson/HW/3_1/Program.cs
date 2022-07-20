4d10a141ca7bc4bb32fac581bb75c2aed1f3c56f
unknown <mir@example.com>
2022-07-20 18:52:13 +0300

    HW 3_1

--- 3_lesson/HW/3_1/Program.cs
+++ 3_lesson/HW/3_1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+void polindrome (string s )
+{
+    for (int i = 0; i < s.Length / 2; ++i)
+        if (s[i] != s[s.Length - 1 - i]) Console.WriteLine ("не палиндром");
+        else Console.WriteLine ("палиндром");
+};
+var v = 14212;
+var s = v.ToString();
+polindrome (s);
